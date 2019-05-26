42fe34c14ec197f460000bcbe6a4c2114d1d8332
hch <2716911186@qq.com>
2019-05-27 00:10:51 +0800

    更新

--- common.js
+++ common.js
@@ -1,20 +1,45 @@
 layui.use(['element','layer','jquery'],function(){
 	var element = layui.element,
 	layer = layui.layer,
-	$ = layui.jquery
+	$ = layui.jquery;
 
-	$(".left-to-right").click(function(){
-		var leftnav = $(".left-nav");
-		var body = $(".body-nav");
-		var left = $(".left");
-		if(!leftnav.hasClass('left')){
-			leftnav.stop().animate({left:"-200px"},"slow");
-			body.stop().animate({left:"0px"}, "slow");
-			leftnav.addClass('left');
-		}else{
-			leftnav.stop().animate({left:"0px"});
-			body.stop().animate({left:"200px"});
-			leftnav.removeClass('left');
-		}
-	})
+    //处理tab的首项不知道如何处理
+    // if($('.first')){
+    // 	$('.first').removeAttr('lay-allowClose')
+    // }
+    
+    //监听toggle点击
+    $('.top-nav .layui-icon-shrink-right').click(function(){
+        if($(this).hasClass('layui-icon-shrink-right')){
+        	$('.left-nav').stop().animate({left:"-200px"},200);
+        	$('.toggle').stop().animate({left:"0px"},200);
+        	$(this).removeClass('layui-icon-shrink-right').addClass('layui-icon-spread-left');
+        }else{
+        	$('.left-nav').stop().animate({left:"0px"},200);
+        	$('.toggle').stop().animate({left:"200px"},200);
+        	$(this).removeClass('layui-icon-spread-left').addClass('layui-icon-shrink-right');
+        }
+    });
+    //监听nav点击
+    element.on('nav(nav)',function(elem){
+    	var id = $(this).attr('lay-id');
+    	var url = $(this).attr('lay-url');
+    	var title = $(this).text();
+        
+    	if(!id){return false;}
+
+    	var active = $('.toggle').find('li[lay-id='+id+']');
+    	if(active.length>0){
+    		element.tabChange('tab',id);
+    	}else{
+    		element.tabAdd('tab',{
+    			title:title,
+    			url: url,
+    			id:id
+    		});
+    		element.tabChange('tab',id);
+    	}
+    	
+
+    });
 })
